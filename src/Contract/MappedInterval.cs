9b3bdd4b685f1d6dfbd1ae156d5230ac67515a9b
Denys Mentiei <denys.mentiei@gmail.com>
2019-03-21 08:35:40 +0200

    MappedInterval equality members.

--- src/Contract/MappedInterval.cs
+++ src/Contract/MappedInterval.cs
@@ -1,6 +1,9 @@
-﻿namespace Contract
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contract
 {
-    public struct MappedInterval<TPayload>
+    public struct MappedInterval<TPayload> : IEquatable<MappedInterval<TPayload>>
     {
         public MappedInterval(long intervalStart, long intervalEnd, TPayload payload)
         {
@@ -8,11 +11,43 @@
             IntervalEnd = intervalEnd;
             Payload = payload;
         }
-        
+
         public long IntervalStart { get; }
 
         public long IntervalEnd { get; }
 
         public TPayload Payload { get; }
+
+        public bool Equals(MappedInterval<TPayload> other)
+        {
+            return IntervalStart == other.IntervalStart && IntervalEnd == other.IntervalEnd && EqualityComparer<TPayload>.Default.Equals(Payload, other.Payload);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is MappedInterval<TPayload> other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = IntervalStart.GetHashCode();
+                hashCode = (hashCode * 397) ^ IntervalEnd.GetHashCode();
+                hashCode = (hashCode * 397) ^ EqualityComparer<TPayload>.Default.GetHashCode(Payload);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(MappedInterval<TPayload> left, MappedInterval<TPayload> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(MappedInterval<TPayload> left, MappedInterval<TPayload> right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
