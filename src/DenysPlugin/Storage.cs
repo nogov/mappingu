f0fe07d0d6dbbed858ae796b092056c502895625
Denys Mentiei <denys.mentiei@gmail.com>
2019-03-21 15:34:56 +0200

    Added denys plugin.

---
+++ src/DenysPlugin/Storage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Contract;
+
+namespace DenysPlugin
+{
+    internal sealed class Storage<T> : IMappedIntervalsCollection<T>
+    {
+        public int Count { get; }
+
+        public void Put(IReadOnlyList<MappedInterval<T>> newIntervals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long from, long to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<MappedInterval<T>> GetEnumerator(long @from)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<MappedInterval<T>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
