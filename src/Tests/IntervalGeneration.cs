a641cd8ac8e98d4e3194e5250a3b835dc1b10260
Denys Mentiei <denys.mentiei@gmail.com>
2019-03-21 09:19:27 +0200

    Added MappedIntervalsCollectionFixtureBase.

---
+++ src/Tests/IntervalGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Contract;
+
+namespace Tests
+{
+    public static class IntervalGeneration
+    {
+        public static IEnumerable<MappedInterval<T>> DashedSequence<T>(long from, long step, int count, Func<int, T> makePayload)
+        {
+            return Sequence(from, step >> 1, step, count, makePayload);
+        }
+
+        public static IEnumerable<MappedInterval<T>> Sequence<T>(long from, long duration, long step, int count, Func<int, T> makePayload)
+        {
+            var time = from;
+            for (var i = 0; i < count; ++i)
+            {
+                yield return new MappedInterval<T>(time, time + duration, makePayload(i));
+                time += step;
+            }
+        }
+    }
+}
---
+++ src/Tests/MappedIntervalsCollectionFixtureBase.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Contract;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public abstract class MappedIntervalsCollectionFixtureBase
+    {
+        private IMappedIntervalsCollection<Crate> _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = CreateSut();
+        }
+
+        [Test]
+        public void NonOverwritingUpdate()
+        {
+            var input = IntervalGeneration.DashedSequence(0, 1, 10, MakeDummy).ToArray();
+
+            _sut.Put(input);
+
+            var output = _sut.ToArray();
+            CollectionAssert.AreEqual(input, output);
+        }
+
+        [Test]
+        public void OverwritingUpdate()
+        {
+            var input = IntervalGeneration.Sequence(0, 10, 5, 5, MakeDummy).ToArray();
+
+            var space = new MappedInterval<Crate>[1];
+            foreach (var i in input)
+            {
+                space[0] = i;
+                _sut.Put(space);
+            }
+
+            var output = _sut.ToArray();
+            Assert.That(output.Length, Is.EqualTo(1));
+            Assert.That(output[0].IntervalStart, Is.EqualTo(input[0].IntervalStart));
+            Assert.That(output[0].IntervalEnd, Is.EqualTo(input[input.Length - 1].IntervalEnd));
+        }
+
+        [TestCase(0)]
+        [TestCase(10)]
+        public void SingleRemove(int margin)
+        {
+            var i = new MappedInterval<Crate>(0, 100, MakeDummy(0));
+            _sut.Put(new [] { i });
+
+            _sut.Delete(i.IntervalStart - margin, i.IntervalEnd + margin);
+
+            Assert.That(_sut.Count, Is.Zero);
+        }
+
+        [Test]
+        public void PartialRemove()
+        {
+            var i = new MappedInterval<Crate>(0, 1000, MakeDummy(0));
+            _sut.Put(new []{ i });
+
+            var gapFrom = 100;
+            var gapTo = 400;
+
+            _sut.Delete(gapFrom, gapTo);
+
+            var output = _sut.ToArray();
+            Assert.That(output.Length, Is.EqualTo(2));
+            Assert.That(output[0].Payload.Value, Is.EqualTo(0));
+            Assert.That(output[0].IntervalStart, Is.EqualTo(i.IntervalStart));
+            Assert.That(output[0].IntervalEnd, Is.EqualTo(gapFrom));
+            Assert.That(output[1].Payload.Value, Is.EqualTo(0));
+            Assert.That(output[1].IntervalStart, Is.EqualTo(gapTo));
+            Assert.That(output[1].IntervalEnd, Is.EqualTo(i.IntervalEnd));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(0, 5)]
+        [TestCase(0, 6)]
+        public void EnumerationFrom(int intervalsToSkip, int margin)
+        {
+            const int count = 10;
+            const int duration = 5;
+            const int step = 5;
+
+            var input = IntervalGeneration.Sequence(0, duration, step, count, MakeDummy).ToArray();
+            _sut.Put(input);
+
+            var from = (duration + step) * intervalsToSkip + margin;
+            var later = input.Where(i => i.IntervalStart >= from).ToArray();
+
+            var part = CollectFrom(_sut, from).ToArray();
+            Assert.That(part.Length, Is.EqualTo(later.Length));
+            CollectionAssert.AreEqual(part, later);
+        }
+
+        protected abstract IMappedIntervalsCollection<Crate> CreateSut();
+
+        protected static IEnumerable<MappedInterval<T>> CollectFrom<T>(IMappedIntervalsCollection<T> collection, long from)
+        {
+            using (var e = collection.GetEnumerator(from))
+            {
+                while (e.MoveNext())
+                {
+                    yield return e.Current;
+                }
+            }
+        }
+
+        protected static Crate MakeDummy(int value)
+        {
+            return new Crate(value);
+        }
+
+        protected sealed class Crate
+        {
+            public Crate(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; }
+        }
+    }
+}
