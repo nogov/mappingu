5d6822b90f917319e4d8aa1c0d52acb077b556a4
Volodymyr <vl.dombrovsky@gmail.com>
2019-03-22 12:31:24 +0200

    moved DenysPlugin into plugins folder

---
+++ src/plugins/DenysPlugin/PluginFactory.cs
@@ -0,0 +1,19 @@
+﻿using Contract;
+
+namespace DenysPlugin
+{
+    //public sealed class PluginFactory : SandboxPlugin
+    //{
+    //    public PluginFactory(ILogger logger)
+    //        : base(logger)
+    //    {
+    //    }
+
+    //    public override string Name => "DenysPlugin";
+
+    //    public override IMappedIntervalsCollection<T> CreateCollection<T>()
+    //    {
+    //        return new Storage<T>();
+    //    }
+    //}
+}
