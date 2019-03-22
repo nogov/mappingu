4072c11f0473667a357f7c6aab8ecbc622c2601a
Denys Mentiei <denys.mentiei@gmail.com>
2019-03-22 13:25:03 +0200

    Added ability to specify plugin names to be included via command-line.

--- src/MappedIntervalsCollection/Program.cs
+++ src/MappedIntervalsCollection/Program.cs
@@ -1,14 +1,16 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Contract;
 using MappedIntervalsCollection;
 
 namespace Console
 {
     internal sealed class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var logger = new ConsoleLogger();
-            var plugins = MefDlyaBednyx.GetPlugins(logger).ToArray();
+            var plugins = FilterIfNeeded(MefDlyaBednyx.GetPlugins(logger), args).ToArray();
 
             var tests = new Tests.Suite(logger, plugins);
             tests.Run();
@@ -17,6 +19,17 @@ namespace Console
             benchmarks.Run();
 
             System.Console.ReadKey();
+        }
+
+        private static IEnumerable<SandboxPlugin> FilterIfNeeded(IEnumerable<SandboxPlugin> plugins, string[] names)
+        {
+            if (names.Length == 0)
+            {
+                return plugins;
+            }
+
+            var needles = new HashSet<string>(names);
+            return plugins.Where(p => needles.Contains(p.Name));
         }
     }
 }
--- src/plugins/DenysPlugin/PluginFactory.cs
+++ src/plugins/DenysPlugin/PluginFactory.cs
@@ -2,18 +2,18 @@
 
 namespace DenysPlugin
 {
-    //public sealed class PluginFactory : SandboxPlugin
-    //{
-    //    public PluginFactory(ILogger logger)
-    //        : base(logger)
-    //    {
-    //    }
+    public sealed class PluginFactory : SandboxPlugin
+    {
+        public PluginFactory(ILogger logger)
+            : base(logger)
+        {
+        }
 
-    //    public override string Name => "DenysPlugin";
+        public override string Name => "DenysPlugin";
 
-    //    public override IMappedIntervalsCollection<T> CreateCollection<T>()
-    //    {
-    //        return new Storage<T>();
-    //    }
-    //}
+        public override IMappedIntervalsCollection<T> CreateCollection<T>()
+        {
+            return new Storage<T>();
+        }
+    }
 }
